81b9408109effff2b68fdab264171d37e1b12c45
Christina <cdrachuk@gmail.com>
2023-06-14 23:11:29 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var yourName = prompt("Enter your name:");
+var bcaTeacher = prompt("Enter a BCA teacher:");
+var subject = prompt("Enter a school subject:");
+var adjective = prompt("Enter an adjective:");
+var timeFrame = prompt("Enter an amount of time:");
+var occupation = prompt("Enter an occupation:");
+var animal = prompt("Enter a barn animal:");
+var number = prompt("Enter a number:");
+var noun = prompt("Enter a noun:");
+var verbIng = prompt("Enter a verb ending in -ing:");
+
+document.write("BCA is obviously " + yourName + "'s favorite place, and " + bcaTeacher+  " is their " +
+" favorite " + subject + " teacher! On the first day of school, " + yourName + " walked in and sat in "
++ " the very front row. The desk had a(n) " + adjective + " pile of paper on it. " + bcaTeacher +
+" said that everyone mut fill out their papers in " + timeFrame + ", but that they had to read through all of the instructions first. " +
+yourName + " decided that they would read the instructions as they go, as that was the most efficient option." +
+" There were questions asking the student's dream job, which " + yourName + " filled out as " + occupation + 
+", and dream pet which " + yourName + " filled out as " + animal + ". At the end of the pile of papers that took " +
+yourName + " " + number + " times as much time to fill out as was alotted, the paper stated that all the student had to do for a 100% was draw a(n) " +
+noun + " on the paper. " + yourName + " started " + verbIng + ".");
